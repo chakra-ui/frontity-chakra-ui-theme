c9705aa32dba151718e5e3bf3d9892112fa82f68
Sage <joseshegs@gmail.com>
2020-01-16 14:07:51 +0100

    hook: refactor use-scroll-progress hook for simplicity

--- packages/chakra-theme/src/components/hooks/useScrollProgress.js
+++ packages/chakra-theme/src/components/hooks/useScrollProgress.js
@@ -1,47 +1,73 @@
 import { useRef, useLayoutEffect, useEffect, useState } from "react";
 
+// check if we're in a browser or server environment
 const isBrowser = typeof window !== `undefined`;
+
+// use the correct effect
 const useEnhancedEffect = isBrowser ? useLayoutEffect : useEffect;
 
+// get window scroll position
 function getScrollPosition() {
   if (!isBrowser) return { x: 0, y: 0 };
   return { x: window.scrollX, y: window.scrollY };
 }
 
 function useScrollEffect(effect) {
-  const scrollPos = useRef(getScrollPosition());
-
-  let timeoutId = null;
+  // ref to keep track of timeout id
+  const timeoutRef = useRef(null);
 
   const update = () => {
-    const currPos = getScrollPosition();
-    effect && effect(currPos);
-    scrollPos.current = currPos;
-    timeoutId = null;
+    // get new scroll position
+    const position = getScrollPosition();
+
+    // run effect with current position
+    effect && effect(position);
   };
 
   useEnhancedEffect(() => {
+    // run effect on mount
     update();
+
+    // run effect as well scroll every 200ms
     const handleScroll = () => {
-      if (timeoutId == null) timeoutId = setTimeout(update, 200);
+      timeoutRef.current = setTimeout(update, 200);
     };
+
+    // attach scroll listener
     window.addEventListener("scroll", handleScroll);
+
     return () => {
+      // perf: remove the event listener
       window.removeEventListener("scroll", handleScroll);
-      if (timeoutId) clearTimeout(timeoutId);
+
+      // perf: if timeout still exists, clear it
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
     };
   }, []);
 }
 
 function useScrollProgress() {
+  // create ref to track node's scrollHeight
   const ref = useRef(null);
+
+  // state to store the scroll progress
   const [progress, setProgress] = useState(0);
 
+  // let's use the sweet effect :)
   useScrollEffect(scrollPos => {
+    // if there's no node, don't do anything 'coz there's no point
+    if (!ref.current) return;
+
+    // else, calculate the scroll percent
     const percent = (scrollPos.y / ref.current.scrollHeight) * 100;
+
+    // update the state
     setProgress(percent);
   });
 
+  // return the goods :)
   return [ref, progress];
 }
 
